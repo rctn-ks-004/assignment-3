724663c095021e705b5df23c99bd1cad8957a532
Tata Sulysta <tata_ss@softwareseni.com>
2022-09-26 15:52:03 +0700

    finalize education section

--- assignment3/src/components/Lists/index.jsx
+++ assignment3/src/components/Lists/index.jsx
@@ -7,13 +7,29 @@ export default class Lists extends Component {
 		const { title, subtitle, description, date } = this.props.data[0];
 		return (
 			<ul className={styles['list--container']}>
-				<li className={styles['list']}>
-					<div className={styles['left--container']}>
-						<h2>{title}</h2> <h3>{subtitle}</h3>
-						<p>{description}</p>
-					</div>
-					<div className={styles['right--container']}>{date}</div>
-				</li>
+				{this.props.data.length > 1 ? (
+					<>
+						{this.props.data.map(({ title, subtitle, description, date }) => {
+							return (
+								<li className={styles['list']}>
+									<div className={styles['left--container']}>
+										<h2>{title}</h2> <h3>{subtitle}</h3>
+										<p>{description}</p>
+									</div>
+									<div className={styles['right--container']}>{date}</div>
+								</li>
+							);
+						})}
+					</>
+				) : (
+					<li className={styles['list']}>
+						<div className={styles['left--container']}>
+							<h2>{title}</h2> <h3>{subtitle}</h3>
+							<p>{description}</p>
+						</div>
+						<div className={styles['right--container']}>{date}</div>
+					</li>
+				)}
 			</ul>
 		);
 	}
