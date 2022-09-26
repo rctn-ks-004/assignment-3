ddd65a7d559b8f9f8a73a77092ae6c3846be5465
Tata Sulysta <tata_ss@softwareseni.com>
2022-09-26 11:57:42 +0700

    add homepage, fix text align in nav

--- assignment3/src/App.js
+++ assignment3/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-import Navbar from './components/Navbar';
+import Homepage from './pages/HomePage';
 
 function App() {
 	return (
-		<div className="App">
-			<Navbar />
+		<div>
+			<Homepage />
 		</div>
 	);
 }
---
+++ assignment3/src/pages/HomePage/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Navbar from '../../components/Navbar';
+
+export default class Homepage extends Component {
+	render() {
+		return (
+			<>
+				<Navbar />
+			</>
+		);
+	}
+}
