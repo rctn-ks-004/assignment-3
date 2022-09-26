f310ed005c2e2501fd46011b19bb05641b15a6e0
Tata Sulysta <tata_ss@softwareseni.com>
2022-09-26 15:20:05 +0700

    fix semantic html on container

--- assignment3/src/components/Container/index.jsx
+++ assignment3/src/components/Container/index.jsx
@@ -4,11 +4,11 @@ import styles from './styles.module.css';
 export default class Container extends Component {
 	render() {
 		return (
-			<div className={styles.container}>
+			<section className={styles.container}>
 				<h1>{this.props.title}</h1>
 
 				<div>{this.props.children}</div>
-			</div>
+			</section>
 		);
 	}
 }
