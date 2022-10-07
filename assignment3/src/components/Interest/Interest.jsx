c917aa301cb5c20c35e51e4159c1152cc9be3e55
mrevanzak <mrevanzak@gmail.com>
2022-10-07 20:57:39 +0700

    feat: interest page

--- assignment3/src/components/Interest/Interest.jsx
+++ assignment3/src/components/Interest/Interest.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Header } from "../Header";
+import useAnimate from "../Hooks";
+import "./interest.css";
 
 const Interest = () => {
-  return (
-    <div>Interest</div>
-  )
-}
+    const { scrollPercent } = useAnimate(80, 100);
 
-export default Interest
+    return (
+        <section className="interest">
+            <Header
+                scrollPercent={scrollPercent === 0 ? 10 : scrollPercent - 60}
+            >
+                Interest
+            </Header>
+            <div className="container">
+                <div className="wrapper">
+                    <div className="card">
+                        <h3>Frontend Development</h3>
+                        <div className="desc">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing
+                            elit. Deserunt, vel?
+                        </div>
+                    </div>
+                    <div className="card">
+                        <h3>Competitive Programming</h3>
+                        <div className="desc">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing
+                            elit. Deserunt, vel?
+                        </div>
+                    </div>
+                    <div className="card">
+                        <h3>Gaming</h3>
+                        <div className="desc">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing
+                            elit. Deserunt, vel?
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Interest;
