52576c8c0d669a3e0bcaf077314b3101704ccf04
ABDILLAH12846 <abdikl479@gmail.com>
2022-10-13 08:54:32 +0700

    first

---
+++ assignment3/src/component/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div class="header">
+        <a href="/start.html">
+          <h1 class="left">AZ</h1>
+        </a>
+
+        <ul>
+          <li><a href="/index.html">Home</a></li>
+          <li><a href="/about.html">About</a></li>
+          <li><a href="/exper.html">Experiences</a></li>
+          <li><a href="/skill.html">Skills</a></li>
+          <li><a href="/interest.html">Interest</a></li>
+          <li><a href="/awward.html">Awwards</a></li>
+        </ul>
+      </div>
+  )
+}
